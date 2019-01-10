7895017bfa1de2b0e3e8d47fa398613735a2ab97
Mohamed Abbassi <mabbassix3@gmail.com>
2019-01-10 13:38:38 +0100

    afficher un message "conversion est fait"

--- InterfaceGraghique/MainInterface.java
+++ InterfaceGraghique/MainInterface.java
@@ -68,7 +68,7 @@ public class MainInterface {
   public void actionPerformed(ActionEvent actionEvent) {
                                 new HuffmanEncoding().encode(filename[0], filename[0] +"huffman",1);
                                 new HuffmanEncoding().decode(filename[0] +".huffman", filename[0]);
-        JOptionPane.showMessageDialog(frame,"conversion est fait.");
+        JOptionPane.showMessageDialog(frame,"conversion est fait.") ;
       
                             }
                         });
